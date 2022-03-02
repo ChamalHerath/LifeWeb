d9f2ca868299118b05ac7ecf9dc1dcb500f25f54
HerathHMCJ_IT18116748 <it18116748@my.sliit.lk>
2022-03-02 16:05:38 +0530

    HealthSection Updated

--- resources/views/pages/health.blade.php
+++ resources/views/pages/health.blade.php
@@ -5,35 +5,48 @@
 @section('content')
 
 
-<!-- ======= Breadcrumbs ======= -->
-<section id="breadcrumbs" class="breadcrumbs">
-    <div class="container">
-        <div class="d-flex justify-content-between align-items-center">
-            <h2>Health</h2>
-            <ol>
-                <li><a href="/">Home</a></li>
-                <li>Our Products</li>
-                <li>Health</li>
-            </ol>
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center">
+                <h2>Health</h2>
+                <ol>
+                    <li><a href="/">Home</a></li>
+                    <li>Our Products</li>
+                    <li>Health</li>
+                </ol>
+            </div>
         </div>
-    </div>
-</section>
-<!-- End Breadcrumbs -->
+    </section>
+    <!-- End Breadcrumbs -->
 
-<!-- ======= About Section ======= -->
-<section id="about" class="">
-    <div class="container">
-        <div class="section-header">
-            <h2>Health</h2>
+    <!-- ======= About Section ======= -->
+    <section id="about" class="">
+        <div class="container">
+            <div class="section-header">
+                <h2>Health</h2>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 about-img">
+                    <img src="assets/img//health.jpg" alt="">
+                </div>
+
+                <div class="col-lg-8 content">
+                    <ul>
+                        <li><i class="fa fa-check-circle"></i> Since we stand on the sentiment that <strong>“Good Health Good
+                                Life”</strong> ,
+                            it is confined to ensure that our policy holders having a good life as a result of good health
+                            refurbish with us. We strongly believe that having a good health nurtured as the major pillar in
+                            success can accomplish many things in life. </li>
+                        <li><i class="fa fa-check-circle"></i> But expenses involve in medical schemes tends to escalate and
+                            we do have sophisticated solutions to ensure your requirements that leads to customize your
+                            protection and ensure that your health receives the best treatment available. </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div class="row">
-            <div class="col-lg-4"></div>
-            <div class="col-lg-4"><h4 style="color: red; text-transform: uppercase; font-size: 20px">Oops!!! Under Construction...</h4></div>
-            <div class="col-lg-4"></div>
-        </div>
-    </div>
-</section><br>
-<!-- End About Section -->
+    </section><br>
+    <!-- End About Section -->
 
 
 @endsection
