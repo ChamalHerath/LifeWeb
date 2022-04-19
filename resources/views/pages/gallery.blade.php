9c5ed58d5473df55d2f05e0d0b7619ca33cb490d
ChamalHerath <chamaljanadara@gmail.com>
2022-04-19 16:44:31 +0530

    Gallery Popup Started

--- resources/views/pages/gallery.blade.php
+++ resources/views/pages/gallery.blade.php
@@ -5,52 +5,76 @@
 @section('content')
 
 
-<section id="breadcrumbs" class="breadcrumbs">
-    <div class="container">
-        <div class="d-flex justify-content-between align-items-center">
-            <h2>Gallery</h2>
-            <ol>
-                <li><a href="/">Home</a></li>
-                <li>Gallery</li>
-            </ol>
+    <section id="breadcrumbs" class="breadcrumbs">
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center">
+                <h2>Gallery</h2>
+                <ol>
+                    <li><a href="/">Home</a></li>
+                    <li>Gallery</li>
+                </ol>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
 
-<!-- ======= About Section ======= -->
-<section id="about" class="">
-    <div class="container">
-        <div class="section-header">
-            <h2>Gallery</h2>
+    <!-- ======= About Section ======= -->
+    <section id="about" class="">
+        <div class="container">
+            <div class="section-header">
+                <h2>Gallery</h2>
+            </div>
+            <div class="row">
+                <div class="col-lg-4"><img id="myImg" src="assets/img/gallery/1.jpg" style="width:100%;max-width:300px">
+
+                    <!-- The Modal -->
+                    <div id="myModal" class="modal">
+                        <span class="close">&times;</span>
+                        <img style="width:100%;max-width:400px" class="modal-content" id="img01">
+                        <div id="caption"></div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;"
+                        src="assets/img/gallery/2.jpg" alt="" srcset=""></div>
+
+                <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;"
+                        src="assets/img/gallery/3.jpg" alt="" srcset=""></div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;"
+                        src="assets/img/gallery/4.jpg" alt="" srcset=""></div>
+                        
+                <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;"
+                        src="assets/img/gallery/5.jpg" alt="" srcset=""></div>
+                <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;"
+                        src="assets/img/gallery/6.jpg" alt="" srcset=""></div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;"
+                        src="assets/img/gallery/7.jpg" alt="" srcset=""></div>
+                <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;"
+                        src="assets/img/gallery/8.jpg" alt="" srcset=""></div>
+                <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;"
+                        src="assets/img/gallery/9.jpg" alt="" srcset=""></div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;"
+                        src="assets/img/gallery/10.jpeg" alt="" srcset=""></div>
+                <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;"
+                        src="assets/img/gallery/11.jpeg" alt="" srcset=""></div>
+                <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;"
+                        src="assets/img/gallery/12.jpeg" alt="" srcset=""></div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;"
+                        src="assets/img/gallery/13.jpeg" alt="" srcset=""></div>
+                <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;"
+                        src="assets/img/gallery/14.jpg" alt="" srcset=""></div>
+                <div class="col-lg-4"></div>
+            </div>
         </div>
-        <div class="row">
-            <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;" src="assets/img/gallery/1.jpg" alt="" srcset=""></div>
-            <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;" src="assets/img/gallery/2.jpg" alt="" srcset=""></div>
-            <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;" src="assets/img/gallery/3.jpg" alt="" srcset=""></div>
-        </div>
-        <div class="row">
-            <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;" src="assets/img/gallery/4.jpg" alt="" srcset=""></div>
-            <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;" src="assets/img/gallery/5.jpg" alt="" srcset=""></div>
-            <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;" src="assets/img/gallery/6.jpg" alt="" srcset=""></div>
-        </div>
-        <div class="row">
-            <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;" src="assets/img/gallery/7.jpg" alt="" srcset=""></div>
-            <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;" src="assets/img/gallery/8.jpg" alt="" srcset=""></div>
-            <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;" src="assets/img/gallery/9.jpg" alt="" srcset=""></div>
-        </div>
-        <div class="row">
-            <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;" src="assets/img/gallery/10.jpeg" alt="" srcset=""></div>
-            <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;" src="assets/img/gallery/11.jpeg" alt="" srcset=""></div>
-            <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;" src="assets/img/gallery/12.jpeg" alt="" srcset=""></div>
-        </div>
-        <div class="row">
-            <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;" src="assets/img/gallery/13.jpeg" alt="" srcset=""></div>
-            <div class="col-lg-4"><img style="display: block; height: 80%; width: 100%; object-fit: cover;" src="assets/img/gallery/14.jpg" alt="" srcset=""></div>
-            <div class="col-lg-4"></div>
-        </div>
-    </div>
-</section><br>
-<!-- End About Section -->
+    </section><br>
+    <!-- End About Section -->
 
 
 @endsection
